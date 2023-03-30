7d7d01cd1d6dddeb38a42cfaa0aa6906181d3b35
ArtemPluktus <artempluktus@gmail.com>
2023-03-30 20:21:25 +0200

    del

--- src/js/task-06.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector(`#validation-input`);
-
-input.addEventListener(`blur`, (e) => {
-  input.classList.toggle("valid", input.value.length >= 6);
-  input.classList.toggle("invalid", input.value.length < 6);
-});
