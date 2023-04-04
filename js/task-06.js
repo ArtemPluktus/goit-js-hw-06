cf78cfd330bd3423e9be1efe395c5630f5270e3a
ArtemPluktus <artempluktus@gmail.com>
2023-04-04 17:11:45 +0200

    done

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,12 @@
 const input = document.querySelector(`#validation-input`);
-const inputLength = input.dataset.length;
 
-input.addEventListener(`blur`, (e) => {
-  input.classList.toggle("valid", input.value.length >= inputLength);
-  input.classList.toggle("invalid", input.value.length < inputLength);
-  // if (input.value.length === inputLength) {
-  //   input.classList.add(`valid`);
-  //   input.classList.remove(`invalid`);
-  // } else {
-  //   input.classList.add(`invalid`);
-  //   input.classList.remove(`valid`);
-  // };
+
+input.addEventListener(`blur`, (evt) => {
+  // input.classList.toggle("valid", input.value.length === inputLength);
+  // input.classList.toggle("invalid", input.value.length > inputLength);
+  // input.classList.toggle("invalid", input.value.length < inputLength);
+  input.classList.add(`invalid`);
+  if (evt.target.value.length === Number(evt.target.dataset.length)) {
+    input.classList.replace("invalid", "valid");
+  }
 });
